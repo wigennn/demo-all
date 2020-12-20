b031c13f1caf52f1eac49616ddbf8d063f80371c
18070900 <zett96@163.com>
2020-12-20 22:29:20 +0800

    mybatis-config.xml

--- demo-basic/src/main/java/com/redbyte/platform/demobasic/DemoBasicApplication.java
+++ demo-basic/src/main/java/com/redbyte/platform/demobasic/DemoBasicApplication.java
@@ -1,8 +1,10 @@
 package com.redbyte.platform.demobasic;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@MapperScan("com.redbyte.platform.demobasic.core.dao")
 @SpringBootApplication
 public class DemoBasicApplication {
 
